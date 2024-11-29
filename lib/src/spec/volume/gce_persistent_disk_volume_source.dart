1f721580b180ccb714831833a86cdf9d8908e700
Michael Christens II <m3talsmith@gmail.com>
2024-11-28 20:18:39 -0500

    Make pdName nullable

--- lib/src/spec/volume/gce_persistent_disk_volume_source.dart
+++ lib/src/spec/volume/gce_persistent_disk_volume_source.dart
@@ -1,13 +1,13 @@
 class GCEPersistentDiskVolumeSource {
   /// File system type to mount (e.g., 'ext4', 'xfs', 'ntfs')
   String? fsType;
-  
+
   /// The partition in the volume that you want to mount (optional)
   int? partition;
-  
+
   /// Unique name of the PD resource in GCE
-  String pdName;
-  
+  String? pdName;
+
   /// Read-only if true, read-write otherwise
   bool? readOnly;
 
@@ -19,9 +19,9 @@ class GCEPersistentDiskVolumeSource {
   }
 
   Map<String, dynamic> toMap() => {
-    if (fsType != null) 'fsType': fsType,
-    if (partition != null) 'partition': partition,
-    'pdName': pdName,
-    if (readOnly != null) 'readOnly': readOnly,
-  };
+        if (fsType != null) 'fsType': fsType,
+        if (partition != null) 'partition': partition,
+        'pdName': pdName,
+        if (readOnly != null) 'readOnly': readOnly,
+      };
 }
